88bfef8f48265212a71a8a50667d3b0a7673b7b7
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-08-14 18:41:18 +0600

    favicon added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,52 +1,52 @@
-import { faTasks } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { Link } from "react-router-dom";
-import logo from "./logo.png";
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 export default function Navbar() {
-	return (
-		<nav className="px-lg-5 navbar navbar-expand-lg navbar-light bg-light shadow shadow-lg mb-5">
-			<div className="container-fluid">
-				<a className="navbar-brand text-success" href="#">
-					<span>
-						<img src={logo} alt="logo" width="35px" />
-					</span>
-					<span>
-						<b className="ms-2">Todo List</b>
-					</span>
-				</a>
-				<a
-					href="#"
-					className="d-lg-none"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-				>
-					<FontAwesomeIcon icon={faTasks} className="fa-2x text-dark" />
-				</a>
-				<div
-					className="ms-auto collapse navbar-collapse"
-					id="navbarSupportedContent"
-				>
-					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-						<li className="nav-item">
-							<Link className="nav-link" to="/mytasks">
-								My Tasks
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/completed">
-								Completed
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/about">
-								About
-							</Link>
-						</li>
-						{/* <li className="nav-item dropdown">
+  return (
+    <nav className="px-lg-5 navbar navbar-expand-lg navbar-light bg-light shadow shadow-lg mb-5">
+      <div className="container-fluid">
+        <a className="navbar-brand text-success" href="#">
+          <span>
+            <img src={logo} alt="logo" width="35px" />
+          </span>
+          <span>
+            <b className="ms-2">Todo List</b>
+          </span>
+        </a>
+        <a
+          href="#"
+          className="d-lg-none"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+        >
+          <FontAwesomeIcon icon={faTasks} className="fa-2x text-dark" />
+        </a>
+        <div
+          className="ms-auto collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/mytasks">
+                My Tasks
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/completed">
+                Completed
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            {/* <li className="nav-item dropdown">
 							<a
 								className="nav-link dropdown-toggle"
 								href="#"
@@ -71,9 +71,9 @@ export default function Navbar() {
 								<li></li>
 							</ul>
 						</li> */}
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
